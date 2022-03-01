47ef4a1d31d615544fc7dc2a75bf501f605e5762
Ivan Nesterov <nesterov.iv2018@gmail.com>
2022-03-01 18:07:20 +0300

    added no solution exception (determinant == 0)

--- lab1/src/NoSolutionException.java
+++ lab1/src/NoSolutionException.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class NoSolutionException {
+public class NoSolutionException extends Exception {
+
+    private final String defaultMessage = "\nThere are no solutions of this system! Determinant of this matrix is zero.";
+
+    @Override
+    public String getMessage() {
+        return defaultMessage;
+    }
+
 }
