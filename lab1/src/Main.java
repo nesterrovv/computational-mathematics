7b6fb38ba6d034dd3757b5a81180d545c5925008
Ivan Nesterov <nesterov.iv2018@gmail.com>
2022-03-02 23:11:13 +0300

    fixed bugs with unsolved systems

--- lab1/src/Main.java
+++ lab1/src/Main.java
@@ -24,16 +24,18 @@ public class Main {
             System.out.println("Your augmented matrix:");
             printMatrix(matrix);
             System.out.println();
+            if (countDeterminantOfMatrix(matrix, matrix.length) == 0) {
+                throw new NoSolutionException();
+            }
             double[] answers = gaussSolution(matrix);
-            System.out.println();
-            System.out.println("Values of variables for your system of linear equations:");
+            System.out.println("Values of variables for your system of linear equations (residuals are included):");
             printVector(answers);
-            System.out.println();
-            System.out.println("\u001B[34m" + "Residual of these calculating = " + calculateResidual(matrix, answers) +
-                    "\u001B[0m");
         } catch (NoSuchElementException noSuchElementException) {
             System.err.println("\u001B[32m" + "Finishing a program..." + "\u001B[0m");
             System.exit(-1);
+        } catch (NoSolutionException noSolutionException) {
+            System.err.println(noSolutionException.getMessage());
+            System.exit(0);
         } catch (Exception exception) {
             System.err.println("Fatal error. Try again.");
             System.exit(1);
@@ -88,6 +90,50 @@ public class Main {
         }
     }
 
+    static void getCofactor(double[][] mat, double[][] temp, int p, int q, int n) {
+        int i = 0, j = 0;
+        // Looping for each element
+        // of the matrix
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n; col++) {
+                // Copying into temporary matrix only those element which are
+                // not in given row and column
+                if (row != p && col != q) {
+                    temp[i][j++] = mat[row][col];
+                    // Row is filled, so increase
+                    // row index and reset col index
+                    if (j == n - 1) {
+                        j = 0;
+                        i++;
+                    }
+                }
+            }
+        }
+    }
+
+    public static double countDeterminantOfMatrix(double[][] matrix, int dimension) {
+        int D = 0; // Initialize result
+        final int N = dimension-1;
+        // Base case : if matrix contains single element
+        if (dimension == 1) return matrix[0][0];
+        // To store cofactors
+        double[][] temp = new double[N][N];
+        // To store sign multiplier
+        int sign = 1;
+        // Iterate for each element of first row
+        for (int f = 0; f < dimension; f++) {
+            // Getting Cofactor of mat[0][f]
+            getCofactor(matrix, temp, 0, f, dimension);
+            D += sign * matrix[0][f]
+                    * countDeterminantOfMatrix(temp, dimension - 1);
+            // terms are to be added
+            // with alternate sign
+            sign = -sign;
+        }
+
+        return D;
+    }
+
     public static void showMatrixExample() {
         System.out.println("Your system of linear equations looks like this:");
         System.out.println("a(11) * x(1) + a(12) * x(2) + ... + a(1n) * x(n) = b(1)");
@@ -112,7 +158,6 @@ public class Main {
     }
 
     public static double[][] receiveMatrixFromInput() {
-        Scanner scanner = new Scanner(System.in);
         int bIndex = 1;
         showMatrixExample();
         int dimension = receiveDimension("number of unknown variables");
@@ -137,7 +182,6 @@ public class Main {
     }
 
     public static double[][] generateRandomMatrix() {
-        Scanner scanner = new Scanner(System.in);
         showMatrixExample();
         int dimension = receiveDimension("number of unknown variables");
         double[][] matrix = new double[dimension][dimension + 1];
@@ -154,6 +198,7 @@ public class Main {
 
     public static double[][] receiveMatrixFromFile() {
         int dimension = receiveDimension("number of unknown variables");
+        int counter = 0;
         while (true) {
             try {
                 Scanner pathScanner = new Scanner(System.in);
@@ -163,16 +208,18 @@ public class Main {
                 if (!(file.exists() && file.isFile())) throw new FileNotFoundException();
                 if (!file.canRead()) throw new PermissionDeniedException();
                 Scanner fileScanner = new Scanner(new BufferedReader(new FileReader(file)));
-                double[][] matrix = new double[dimension][dimension];
+                double[][] matrix = new double[dimension][dimension+1];
                 while (fileScanner.hasNextLine()) {
                     for (int i = 0; i < matrix.length; i++) {
                         String[] line = fileScanner.nextLine().trim().split(" ");
                         for (int j = 0; j < line.length; j++) {
+                            System.out.println(line[j]);
                             matrix[i][j] = Double.parseDouble(line[j]);
+                            counter += 1;
+                            if (counter == dimension * dimension + dimension) return matrix;
                         }
                     }
                 }
-                System.out.println();
                 return matrix;
             } catch (FileNotFoundException fileNotFoundException) {
                 System.err.println("File not found! Check path and try enter it again.");
@@ -219,8 +266,7 @@ public class Main {
                 throw new NoSolutionException();
             }
             //Обратный ход (Зануление верхнего правого угла)
-            for (int k = dimension - 1; k > -1; k--) // k is number of line
-            {
+            for (int k = dimension - 1; k > -1; k--) { // k is number of line
                 for (int i = dimension; i > -1; i--) { // i is number of column
                     copyOfMatrix[k][i] = copyOfMatrix[k][i] / matrix[k][k];
                 }
@@ -234,6 +280,20 @@ public class Main {
                 for (int i = 0; i < dimension; i++) {
                     answers[i] = copyOfMatrix[i][dimension];
                 }
+                double[] residuals = new double[answers.length];
+                for (int i = matrix.length - 1;  i >= 0; i--) {
+                    double b = matrix[i][matrix[0].length - 1];
+                    double sum = 0;
+                    for (int j = 0; j < matrix[0].length - 1; j++) {
+                        sum += answers[j] * matrix[i][j];
+                    }
+                    residuals[i] = b - sum;
+                }
+                System.out.println("\u001B[34m" + "\nResiduals:");
+                for (int index = 0; index < residuals.length; index++) {
+                    System.out.println("For x(" + index + 1 + "): " + residuals[index]);
+                }
+                System.out.println("\u001B[0m");
                 return answers;
             }
         } catch (NoSolutionException noSolutionException) {
@@ -243,11 +303,4 @@ public class Main {
         return null;
     }
 
-    public static double calculateResidual(double[][] matrix, double[] variables) {
-        double temporaryValue = 0;
-        for (int j = 0; j < matrix[0].length - 1; j++) {
-            temporaryValue += (matrix[0][j] * variables[j]);
-        }
-        return matrix[0][matrix[0].length - 1] - temporaryValue;
-    }
 }
