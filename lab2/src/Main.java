e5eaebd77c98f286735e7a022e74e6b3d5bd8289
Ivan Nesterov <red@vt.com>
2022-03-31 14:44:20 +0300

    added system for demonsrtation code woriking:

--- lab2/src/Main.java
+++ lab2/src/Main.java
@@ -1,10 +1,31 @@
+import methods.BisectionMethodSolver;
+import methods.ChordMethodSolver;
 import methods.SimpleIterationMethodSolver;
 
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Lab work #2 demonstrating system is started!");
+        System.out.println("\n\n\n\n\n");
+        System.out.println("-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-");
+        System.out.println("Task #1.1. Usage of bisection method for nonlinear equations solving:");
+        System.out.println("-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-");
+        BisectionMethodSolver bisectionMethodSolver = new BisectionMethodSolver();
+        bisectionMethodSolver.solveEquation();
+        System.out.println("\n\n\n\n\n");
+        System.out.println("-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-");
+        System.out.println("Task #1.2. Usage of chord method for nonlinear equations solving:");
+        System.out.println("-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-");
+        ChordMethodSolver chordMethodSolver = new ChordMethodSolver();
+        chordMethodSolver.solveEquation();
+        System.out.println("\n\n\n\n\n");
+        System.out.println("-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-");
+        System.out.println("Task #2. Usage of fixed point iteration method for nonlinear equations system solving::");
+        System.out.println("-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-|-");
         SimpleIterationMethodSolver simpleIterationMethodSolver = new SimpleIterationMethodSolver();
         simpleIterationMethodSolver.solveSystemOfEquations();
+        System.out.println("\n\n\n\n\n");
+        System.out.println("Lab work #2 demonstrating system is finished!");
 
     }
 }
